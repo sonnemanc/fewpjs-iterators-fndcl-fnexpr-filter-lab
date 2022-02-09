8b1c4e91e64af1bef52cfd22ab82bf8c81a03081
Corban <sonnemanc@gmail.com>
2022-02-09 12:32:41 -0800

    commit tree d448395bc1dadb32fbd4438ff91486013c5d77cf

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, c) {
+  return drivers.filter(n =>
+    n.toLowerCase() === c.toLowerCase()
+  )
+}
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(x => {
+    let firstChar = x.charAt(0)
+      return str.charAt(0) === firstChar
+  })
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(n => {
+      return n.name === str;
+    });
+}
